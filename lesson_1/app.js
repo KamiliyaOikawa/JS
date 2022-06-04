99736c3b11c25fe8795afe54f0daa4b2f1a408ef
Oikawa <knasyrova622@gmail.com>
2022-06-04 06:22:18 -0700

    lesson_1

---
+++ lesson_1/app.js
@@ -0,0 +1,43 @@
+const a = "A";
+
+let c = 32;
+
+c = 21
+const bool = true;
+const bool2 = false;
+console.log(c, a)
+
+console.log(typeof a)
+
+const obj = { //obj
+    key: "value",
+    username: 'Aidana',
+    data: {
+        last_name: "jack"
+    }
+}
+console.log(obj.username)
+console.log(obj.data.last_name)
+
+const arr = ['data', 21, true]
+
+const first_num = prompt('Первое число :')
+const second_num = prompt('Второе число :')
+
+if (first_num < second_num) {
+    console.log(first_num + '<' + second_num)
+} else if (first_num > second_num) {
+    console.log(first_num + '>' + second_num)
+} else {
+    console.log(first_num + '=' + second_num)
+}
+const age = prompt('Сколько вам лет :')
+
+if (age < 18) {
+    console.log('Not')
+} else if (age > 18) {
+    console.log('yes')
+} else {
+    console.log('Her ego')
+}
+console.log()
